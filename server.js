06772e7eb580f482f75b8e1a9b86d636ff74d81c
James <jwin4740@gmail.com>
2017-03-20 16:16:39 -0400

    linked css to handlebars

--- server.js
+++ server.js
@@ -30,6 +30,7 @@ app.set("view engine", "handlebars");
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static(__dirname + "/public"));
+// app.use(express.static(__dirname + "/views"));
 
 
 // connects to mysql database
