d1ee7b56c705c290758ce1a99707a8c018a49a4e
James <jwin4740@gmail.com>
2017-04-05 11:32:29 -0400

    reconfig

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,7 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  port: 3306,
-  host: "us-cdbr-iron-east-03.cleardb.net",
-  user: "b5c06e7c9887c5",
-  password: "1e809df3",
-  database: "heroku_6b0bbc304e5f86f"
-});
+var connection = mysql.createConnection("mysql://b5c06e7c9887c5:1e809df3@us-cdbr-iron-east-03.cleardb.net/heroku_6b0bbc304e5f86f?reconnect=true");
 
 // Make connection.
 connection.connect(function(err) {
